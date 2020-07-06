25ef3392cc819f8473658ebf99da20e12a8b0442
Michael Waide <mvwaide@gmail.com>
2020-07-06 19:48:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
-class Person  # starts defining our Person class
-  def talk  # start defining our #talk method
-    puts "Hello World!"  # prints out 'Hello World!' to the output
-  end  # closes out our definition of the #talk method. Super important.
-end  # closes out our definiti
+class Person  
+  def talk  
+    puts "Hello World!"  
+  end  
+end  
